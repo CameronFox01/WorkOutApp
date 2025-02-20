789baa00e80a1ce2b1bcf9dc5e0656625ac65526
Cameron Fox <cameron22401@icloud.com>
2025-02-20 13:16:40 -0700

    This is just the start of the app. It only went from "Hello Wolrd" to "Lets WorkOut"

---
+++ WorkoutApp/WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by Cameron Fox on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
