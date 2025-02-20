99ccf72a4a6ba9241ff41396c22183b00161d5b5
Cameron Fox <cameron22401@icloud.com>
2025-02-20 13:46:33 -0700

    I have set up the buttons (in the wrong place) for the bottom selection to go back and fourth from section to section

--- WorkoutApp/WorkoutApp/ContentView.swift
+++ WorkoutApp/WorkoutApp/ContentView.swift
@@ -10,12 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "dumbbell")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.black)
+                .font(.largeTitle)
+            Text("Let's WorkOut!!")
+            
+        }
+        HStack{
+            Button(action: {
+                print("OverView of Account Pressed")
+            }){
+                pageButtonCreation(label: "person.circle")
+            }
+            
+            Button(action: {
+                print("WorkOut Page Pressed")
+            }){
+                pageButtonCreation(label: "dumbbell")
+            }
+            
+            Button(action: {
+                print("Sport Page Pressed")
+            }){
+                pageButtonCreation(label: "sportscourt")
+            }
+            
+            Button(action: {
+                print("Trophy Page Pressed")
+            }){
+                pageButtonCreation(label: "trophy.circle")
+            }
         }
         .padding()
+    }
+    private func pageButtonCreation(label: String) -> some View {
+        Image(systemName: label)
+            .imageScale(.large)
+            .foregroundStyle(.black)
     }
 }
 
